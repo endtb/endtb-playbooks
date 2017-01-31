13e9e5042942f4afd64673ac895b9d48b8d4b8cd
mseaton <mseaton@pih.org>
2017-01-31 07:25:09 -0500

    Add script to deploy to test server, and test playbook for use in testing connectivity

--- ci/deploy-to-test.sh
+++ ci/deploy-to-test.sh
@@ -2,6 +2,7 @@
 
 PLAYBOOK_NAME="$1"
 SSH_USER="$2"
+SSH_KEY_FILE="$3"
 
 yum install -y ansible
-ansible-playbook --sudo -i hosts -u "$SSH_USER" "playbooks/$PLAYBOOK_NAME.yml" --limit "endtb-test" -vvvv
+ansible-playbook --sudo -i hosts -u "$SSH_USER" --private-key="$SSH_KEY_FILE" "playbooks/$PLAYBOOK_NAME.yml" --limit "endtb-test" -vvvv
