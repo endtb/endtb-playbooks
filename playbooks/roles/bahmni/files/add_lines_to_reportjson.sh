f68a761578bba04bff6d440c0c9d7aa0b8eef0cf
James Mbabazi <jmbabazi@pih.org>
2018-01-30 09:40:15 -0500

    implementation specific configurations for reports

---
+++ playbooks/roles/bahmni/files/add_lines_to_reportjson.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo '},' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo " "  >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo '  "CultureResults": { ' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo '     "name": "Culture Results(Excel)", ' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo '     "type": "MRSGeneric", ' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo '     "config": { ' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo '     "sqlPath": "/var/www/bahmni_config/openmrs/apps/reports/sql/cultureResults.sql", ' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo '     "macroTemplatePath" : "/var/www/bahmni_config/openmrs/apps/reports/macroTemplates/Empty.xls" ' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo '   } ' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo '  } ' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo " " >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
+echo '} ' >> /var/www/bahmni_config/openmrs/apps/reports/reports.json
