7b5d76df45a2322a0a8fdb968fff863ab865c49a
James Mbabazi <jmbabazi@pih.org>
2018-01-30 09:40:07 -0500

    implementation specific configurations for reports

---
+++ playbooks/roles/bahmni/files/delete_last_lines_reportsjson.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ #sed -i '$ s/.$//' /var/www/bahmni_config/openmrs/apps/reports/reports.json
+ sed -i '$d' /var/www/bahmni_config/openmrs/apps/reports/reports.json
+ sed -i '$d' /var/www/bahmni_config/openmrs/apps/reports/reports.json
+ sed -i '$d' /var/www/bahmni_config/openmrs/apps/reports/reports.json
